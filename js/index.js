9552a314b8353358a1a923a4a8de458d9111d0fd
Utkarsh Patil <utkarsh2305@gmail.com>
2017-06-27 01:28:56 +0400

    Create navbar

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
+	$(".button-collapse").sideNav();
+	$('.materialboxed').materialbox();
 	particlesJS.load('particles-js', 'js/particles.json', function() {
 		console.log('callback - particles.js config loaded');
 	});
 	$(function(){
 		$(".name-print").typed({
 			strings: [
-				"<span style=\"color: #e57373;\">C</span>  printf(\"I'm Utkarsh.\");",
-				"<span style=\"color: #9fa8da;\">C++</span>  cout &lt;&lt; \"I'm Utkarsh.\"",
-				"<span style=\"color: #2196f3;\">Python</span>  print \"I'm Utkarsh.\"",
-				"<span style=\"color: #8bc34a;\">HTML</span> &lt;p&gt;\"I'm Utkarsh.\"&lt;/p&gt;",
-				"<span style=\"color: #ff3d00;\">Javascript</span>  alert(\"I'm Utkarsh.\");",
-				"<span style=\"color: #a1887f;\">Java</span>  System.out.println(\"I'm Utkarsh.\");",
-				"<span style=\"color: #d50000;\">Ruby</span>  puts \"I'm Utkarsh.\""
+				"I'm Utkarsh. ^2000Feel free to explore my website.",
 			],
-			typeSpeed: 45,
-			loop: true,
-			backDelay: 2000,
+			typeSpeed: 30,
 			shuffle: true
 		});
 	});
+});
+
+$(window).scroll(function() {
+	if ($(window).scrollTop() >= ($(window).height() - $('#particles-js').height())) {
+		$('.navigation-bar').removeClass('transparent').addClass('indigo').addClass('darken-4');
+	}
+	else {
+		$('.navigation-bar').removeClass('indigo').removeClass('darken-4').addClass('transparent');
+	}
 });
